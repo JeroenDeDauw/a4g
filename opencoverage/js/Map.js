287c4c39ad9bdc2f3f0d5abdd1e353dfbe593406
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-05-08 23:05:23 +0200

    get data via http request

--- opencoverage/js/Map.js
+++ opencoverage/js/Map.js
@@ -1,5 +1,6 @@
  
-$().ready(function() {
+(function($) { $( document ).ready( function() {
+	
 	var projection = new OpenLayers.Projection("EPSG:900913");
 	var displayProjection = new OpenLayers.Projection("EPSG:4326");
 	
@@ -14,25 +15,38 @@ $().ready(function() {
 	    displayProjection: displayProjection
 	});
 	
-	var heatMapOverlay = new Heatmap.Layer("Heatmap");
-	
-	for (var latlng in coordinates) {
-		var lonLat = new OpenLayers.LonLat(coordinates[latlng][1], coordinates[latlng][0]);
-		lonLat.transform( displayProjection, projection );
-		heatMapOverlay.addSource( new Heatmap.Source( lonLat ) );
-	}
-	
-	heatMapOverlay.defaultIntensity = 0.1;
-	heatMapOverlay.setOpacity(0.33);
-	
 	map.addLayers([
 		new OpenLayers.Layer.OSM.Mapnik("OSM Mapnik"),
 		new OpenLayers.Layer.OSM.Osmarender("OSM arender"),
-		new OpenLayers.Layer.OSM.CycleMap("OSM Cycle Map"),
+		new OpenLayers.Layer.OSM.CycleMap("OSM Cycle Map")
 		//new OpenLayers.Layer.WMS("OpenLayers WMS", "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'}),
-		heatMapOverlay
 	]);
 	
 	map.addControl(new OpenLayers.Control.LayerSwitcher());
-	map.zoomToExtent(heatMapOverlay.getDataExtent());
-});
+	
+	$.getJSON(
+		'js/Data.js',
+		{
+			
+		},
+		function( data ) {
+			var coordinates = data.coordinates;
+			var heatMapOverlay = new Heatmap.Layer("Heatmap");
+			
+			heatMapOverlay.defaultIntensity = 0.1;
+			heatMapOverlay.setOpacity(0.33);
+			
+			map.addLayers([ heatMapOverlay ]);
+			
+			for (var latlng in coordinates ) {
+				var coord = coordinates[latlng];
+				var lonLat = new OpenLayers.LonLat(coord.lon, coord.lat);
+				lonLat.transform( displayProjection, projection );
+				heatMapOverlay.addSource( new Heatmap.Source( lonLat, null, coord.value ) );
+			}
+			
+			map.zoomToExtent(heatMapOverlay.getDataExtent());
+		}
+	);
+	
+} ); })(jQuery);
