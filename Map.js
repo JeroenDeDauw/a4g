664eb3fef04bf58ba84d8e46d83db7baef2e3783
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-05-08 16:59:56 +0200

    added OSM support

--- Map.js
+++ Map.js
@@ -1,32 +1,38 @@
-var map;
  
 function init() {
-	map = new OpenLayers.Map('map', {
+	var projection = new OpenLayers.Projection("EPSG:900913");
+	var displayProjection = new OpenLayers.Projection("EPSG:4326");
+	
+	var map = new OpenLayers.Map('map', {
 	    controls: [
 	        new OpenLayers.Control.Navigation(),
 	        new OpenLayers.Control.PanZoomBar(),
 	        new OpenLayers.Control.LayerSwitcher({'ascending':false}),
 	        new OpenLayers.Control.MousePosition(),
-	    ]
+	    ],
+	    projection: projection,
+	    displayProjection: displayProjection
 	});
 	
-	var heat = new Heatmap.Layer("Heatmap");
+	var heatMapOverlay = new Heatmap.Layer("Heatmap");
 	
 	for (var latlng in coordinates) {
-	  var point = new Heatmap.Source(new OpenLayers.LonLat(coordinates[latlng][1], coordinates[latlng][0]));
-	  heat.addSource(point);
+		var lonLat = new OpenLayers.LonLat(coordinates[latlng][1], coordinates[latlng][0]);
+		lonLat.transform( displayProjection, projection );
+		heatMapOverlay.addSource( new Heatmap.Source( lonLat ) );
 	}
 	
-	heat.defaultIntensity = 0.1;
-	heat.setOpacity(0.33);
+	heatMapOverlay.defaultIntensity = 0.1;
+	heatMapOverlay.setOpacity(0.33);
 	
-	var shaded = new OpenLayers.Layer.VirtualEarth("VirtualEarth Shaded", {
-	  type: VEMapStyle.Shaded,
-	  animationEnabled: false
-	});
+	map.addLayers([
+		new OpenLayers.Layer.OSM.Mapnik("OSM Mapnik"),
+		new OpenLayers.Layer.OSM.Osmarender("OSM arender"),
+		new OpenLayers.Layer.OSM.CycleMap("OSM Cycle Map"),
+		//new OpenLayers.Layer.WMS("OpenLayers WMS", "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'}),
+		heatMapOverlay
+	]);
 	
-	var wms = new OpenLayers.Layer.WMS("OpenLayers WMS", "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'});
-	 
-	map.addLayers([shaded, wms, heat]);
-	map.zoomToExtent(heat.getDataExtent());
+	map.addControl(new OpenLayers.Control.LayerSwitcher());
+	map.zoomToExtent(heatMapOverlay.getDataExtent());
 }
